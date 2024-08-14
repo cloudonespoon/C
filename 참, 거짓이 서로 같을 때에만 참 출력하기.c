b3f9caac44fdfd2a9908eee4620d6f8b2a2dcfe2
조현강 <165774355+cloudonespoon@users.noreply.github.com>
2024-08-14 21:24:50 +0900

    Add files via upload

---
+++ 참, 거짓이 서로 같을 때에만 참 출력하기.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if ((a == 0) && (b == 0)) printf("1");
+    else if ((a == 1) && (b == 1)) printf("1");
+    else printf("0");
+    return 0;
+}
